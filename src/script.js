600e09386bfe377204ba67601fa7053e7de3c5e1
Franz2610 <francescofroechlich1@gmail.com>
2023-04-11 18:48:40 +0200

    vue-email-list

---
+++ src/script.js
@@ -0,0 +1,27 @@
+const {createApp} = Vue;
+
+createApp({
+    data() {
+        return{
+            title: 'API Email List',
+            randomEmail: [],
+            basePath:'https://flynn.boolean.careers/exercises/api/'
+        }
+    },
+    methods: {
+        getData(){
+            this.randomEmail = [];
+            for(let i = 0; i < 10; i++){
+                axios.get(this.basePath + 'random/mail').then((res)=> {
+                    console.log(res.data.response);
+                    this.randomEmail.push(res.data.response);
+                });
+            }
+        }
+    },
+    mounted(){
+        this.getData();
+    }
+}).mount('#app');
+
+// https://flynn.boolean.careers/exercises/api/random/mail
